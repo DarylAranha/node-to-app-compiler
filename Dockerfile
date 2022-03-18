495dfa9106ce87a9e56fe8a977e8f276451067a8
darylaranha <daryl.aranha@gmail.com>
2022-03-18 18:09:13 -0400

    Script updates

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM alpine:3.15
 
 RUN apk add --no-cache nodejs npm
 
+RUN npm install --prefix /tmp/package-compiler browserify uglify-js
+
 ADD compiler.sh compiler.sh
 
 WORKDIR /app
--- compiler.sh
+++ compiler.sh
@@ -6,22 +6,49 @@ CURRENT_DIR=${PWD}
 FOLDER_NAME="package-compiler"
 PACKAGE_FILE="nodePackage.js"
 COMPILED_FILE="convertedPackage.js"
-
-PACKAGE_NAME=$(echo $1 | tr -dc '[:alnum:]\n\r' | tr '[:upper:]' '[:lower:]')
-VARIABLE_NAME="${PACKAGE_NAME}Compiled"
-COMPRESSED_COMPILED_FILE="${PACKAGE_NAME}CompiledPackage.js"
+PACKAGE_TO_INSTALL=$1
+IS_LOCAL="false"
 
 # Perform operation in tmp folder
 cd $TEMP_DIR && mkdir -p $FOLDER_NAME && cd $FOLDER_NAME
 
-npm install browserify
+# check if option is provided
+# Note: only option available to now is --help and --local
+# by default, the package will be downloaded via npm, if --local is provided then
+# the script will use the in-build package
+if [ -z "$1" ]; then
+    echo -e "Command: \n\t <options> <node_package>"
+    exit
+fi
 
-npm install uglify-js
+PACKAGE_TO_INSTALL=$1
 
-npm install $1
+if [ "$1" = "--help" ]; then
+    echo -e "Command: \n\t <options> <node_package>"
+    echo -e "Options: \n\t --local Search node in-built package. By default, the script will install the package"
+    exit
+fi
+
+if [ "$1" = "--local" ]; then
+    if [ ! -z "$2" ]; then
+        IS_LOCAL="true"
+        PACKAGE_TO_INSTALL=$2
+    else
+        echo -e "Command: \n\t <options> <node_package>"
+        exit
+    fi
+fi
+
+if [ "$IS_LOCAL" == "false" ]; then
+    npm install $PACKAGE_TO_INSTALL
+fi
+
+PACKAGE_NAME=$(echo $PACKAGE_TO_INSTALL | tr -dc '[:alnum:]\n\r' | tr '[:upper:]' '[:lower:]')
+VARIABLE_NAME="${PACKAGE_NAME}Compiled"
+COMPRESSED_COMPILED_FILE="${PACKAGE_NAME}CompiledPackage.js"
 
 # generate a file that import the package
-echo -e "let $VARIABLE_NAME = require('$1');\nmodule.exports = $VARIABLE_NAME;" > $PACKAGE_FILE
+echo -e "let $VARIABLE_NAME = require('$PACKAGE_TO_INSTALL');\nmodule.exports = $VARIABLE_NAME;" > $PACKAGE_FILE
 
 # generate the compiled file
 node node_modules/browserify/bin/cmd.js $PACKAGE_FILE -o $COMPILED_FILE
@@ -31,7 +58,7 @@ node node_modules/browserify/bin/cmd.js $PACKAGE_FILE -o $COMPILED_FILE
 # 2. replace local import of package to variable to global
 # 3. replace all existance of require
 # 4. export the global variable
-sed -i "s/let $VARIABLE_NAME = require('$1');/$VARIABLE_NAME = require('$1');/g" $COMPILED_FILE
+sed -i "s/let $VARIABLE_NAME = require('$PACKAGE_TO_INSTALL');/$VARIABLE_NAME = require('$PACKAGE_TO_INSTALL');/g" $COMPILED_FILE
 sed -i "s/module.exports = $VARIABLE_NAME;//g" $COMPILED_FILE
 sed -i 's/require/fetchPackage/g' $COMPILED_FILE
 echo -e "var $VARIABLE_NAME;\nmodule.exports = $VARIABLE_NAME;" >> $COMPILED_FILE
@@ -41,3 +68,5 @@ node node_modules/uglify-js/bin/uglifyjs  $COMPILED_FILE -c -o $COMPRESSED_COMPI
 
 # move the computer result back to app folder
 cp $COMPRESSED_COMPILED_FILE $CURRENT_DIR
+
+echo -e "Completed.\nUsage: import $VARIABLE_NAME from './$COMPRESSED_COMPILED_FILE';"
